d5588913c910e9ed7ccdd12f084f101227bd16e6
vBital <dc.pereira.work@gmail.com>
2024-04-03 11:24:34 +0100

    Nível 5.1

---
+++ ShippingCompany.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class ShippingCompany here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ShippingCompany extends Transport
+{
+    // instance variables - replace the example below with your own
+    private String name;
+    private ArrayList<Transport> inService;
+
+    /**
+     * Constructor for objects of class ShippingCompany
+     */
+    public ShippingCompany()
+    {
+        
+    }
+
+    
+    
+}
