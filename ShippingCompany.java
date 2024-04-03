18a88899312207f508a17bfd367ff6eaf35e6a15
vBital <dc.pereira.work@gmail.com>
2024-04-03 11:27:53 +0100

    Nível 5.3

--- ShippingCompany.java
+++ ShippingCompany.java
@@ -19,6 +19,7 @@ public class ShippingCompany extends Transport
         this.name = name;
     }
 
-    
+    public String getName(){return name;}   
+    public void setName(String name) {this.name = name;}
     
 }
